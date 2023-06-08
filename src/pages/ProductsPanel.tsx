83d4092f6431772a13d41c70898fb8899c2bb22c
ahsn.dev <ahsn.dev@gmail.com>
2023-06-08 14:17:42 +0330

    add interface and truncateText  to ProductsPanel

--- src/pages/ProductsPanel.tsx
+++ src/pages/ProductsPanel.tsx
@@ -12,104 +12,107 @@ import {
   Image,
 } from "@chakra-ui/react";
 import { FcEditImage, FcEmptyTrash } from "react-icons/fc";
-import mouse from "../assets/products/mouse.png";
-import headphone from "../assets/products/headphone.png";
-import ipad from "../assets/products/ipad.png";
 import AddProductModal from "../components/AddProductModal";
+import axios from "axios";
+import { QueryFunction, useQuery } from "@tanstack/react-query";
 
-const ProductsPanel = () => {
-  const itemsPerPage = 2;
-  const [currentPage, setCurrentPage] = useState(0);
+interface Product {
+  _id: string;
+  subcategory: string;
+  name: string;
+  images: string[];
+  thumbnail: string;
+}
 
-  const data = [
-    {
-      id: 1,
-      image: mouse,
-      name: "Corsair Ironclaw RGB 18000 DPI Optical Gaming Mouse",
-      category: "ماوس",
-    },
-    {
-      id: 2,
-      image: headphone,
-      name: "Beats by Dr. Dre Studio3 Skyline Over-Ear Noise Cancelling Bluetooth Headphones",
-      category: "هدفون",
-    },
-    {
-      id: 3,
-      image: ipad,
-      name: "SteelSeries Apex Pro Backlit Mechanical OmniPoint Gaming Keyboard - English",
-      category: "کیبورد",
-    },
-    {
-      id: 1,
-      image: mouse,
-      name: "Corsair Ironclaw RGB 18000 DPI Optical Gaming Mouse",
-      category: "ماوس",
-    },
-    {
-      id: 2,
-      image: headphone,
-      name: "Beats by Dr. Dre Studio3 Skyline Over-Ear Noise Cancelling Bluetooth Headphones",
-      category: "هدفون",
-    },
-    {
-      id: 3,
-      image: ipad,
-      name: "SteelSeries Apex Pro Backlit Mechanical OmniPoint Gaming Keyboard - English",
-      category: "کیبورد",
-    },
-    {
-      id: 4,
-      image: mouse,
-      name: "Corsair Ironclaw RGB 18000 DPI Optical Gaming Mouse",
-      category: "ماوس",
-    },
-    {
-      id: 5,
-      image: headphone,
-      name: "Beats by Dr. Dre Studio3 Skyline Over-Ear Noise Cancelling Bluetooth Headphones",
-      category: "هدفون",
-    },
-    {
-      id: 6,
-      image: ipad,
-      name: "SteelSeries Apex Pro Backlit Mechanical OmniPoint Gaming Keyboard - English",
-      category: "کیبورد",
-    },
-    {
-      id: 7,
-      image: mouse,
-      name: "Corsair Ironclaw RGB 18000 DPI Optical Gaming Mouse",
-      category: "ماوس",
-    },
-    {
-      id: 8,
-      image: headphone,
-      name: "Beats by Dr. Dre Studio3 Skyline Over-Ear Noise Cancelling Bluetooth Headphones",
-      category: "هدفون",
-    },
-    {
-      id: 9,
-      image: ipad,
-      name: "SteelSeries Apex Pro Backlit Mechanical OmniPoint Gaming Keyboard - English",
-      category: "کیبورد",
-    },
-  ];
+interface ProductsResponse {
+  status: string;
+  page: number;
+  per_page: number;
+  total: number;
+  total_pages: number;
+  thumbnail: string;
+  data: {
+    products: Product[];
+  };
+}
 
-  const totalPages = Math.ceil(data.length / itemsPerPage);
+interface Subcategory {
+  _id: string;
+  category: string;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+  slugname: string;
+}
 
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
+const truncateText = (text: string, limit: number): string => {
+  if (text.length <= limit) {
+    return text;
+  }
+  return text.slice(0, limit) + "...";
+};
+
+const ProductsPanel: React.FC = () => {
+  const itemsPerPage = 3;
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const fetchProducts: QueryFunction<Product[]> = async () => {
+    const [productsResponse, subcategoriesResponse] = await Promise.all([
+      axios.get<ProductsResponse>(
+        "http://localhost:8000/api/products?limit=all"
+      ),
+      axios.get<{ data: { subcategories: Subcategory[] } }>(
+        "http://localhost:8000/api/subcategories?limit=all"
+      ),
+    ]);
+
+    const subcategoriesMap: Record<string, string> =
+      subcategoriesResponse.data.data.subcategories.reduce(
+        (map: Record<string, string>, subcategory) => {
+          map[subcategory._id] = subcategory.name;
+          return map;
+        },
+        {}
+      );
+
+    const products = productsResponse.data.data.products.map((product) => ({
+      ...product,
+      subcategory: subcategoriesMap[product.subcategory] || product.subcategory,
+    }));
+
+    return products;
   };
 
-  const paginatedData = data.slice(
-    currentPage * itemsPerPage,
-    (currentPage + 1) * itemsPerPage
-  );
+  const {
+    data: products = [],
+    isLoading,
+    isError,
+  } = useQuery<Product[]>(["products", "subcategories"], fetchProducts);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error fetching products</div>;
+  }
+
+  const maxPages = Math.ceil(products.length / itemsPerPage);
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentData = products.slice(indexOfFirstItem, indexOfLastItem);
+
+  const handleNextPage = () => {
+    setCurrentPage((prev) => prev + 1);
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage((prev) => prev - 1);
+  };
 
   return (
     <>
-      <HStack className="mb-16 flex justify-between px-4">
+      <HStack className="mb-8 flex justify-between px-4">
         <Text as="h2" className="text-2xl font-bold text-slate-700">
           مدیریت کالاها
         </Text>
@@ -119,54 +122,78 @@ const ProductsPanel = () => {
         <Table variant="striped" colorScheme="twitter">
           <Thead className="bg-blue-300 text-xl">
             <Tr className="text-xl">
-              <Th style={{ fontSize: "18px", color: "#475569" }}>تصویر</Th>
-              <Th style={{ fontSize: "18px", color: "#475569" }}>نام کالا</Th>
-              <Th style={{ fontSize: "18px", color: "#475569" }}>دسته بندی</Th>
               <Th
                 style={{
                   fontSize: "18px",
                   color: "#475569",
-                  paddingRight: "40px",
+                  textAlign: "center",
+                }}
+              >
+                تصویر
+              </Th>
+              <Th style={{ fontSize: "18px", color: "#475569" }}>نام کالا</Th>
+              <Th
+                style={{
+                  fontSize: "18px",
+                  color: "#475569",
+                  textAlign: "center",
+                }}
+              >
+                دسته بندی
+              </Th>
+              <Th
+                style={{
+                  fontSize: "18px",
+                  color: "#475569",
+                  textAlign: "center",
                 }}
               >
                 عملیات
               </Th>
             </Tr>
           </Thead>
-          <Tbody>
-            {paginatedData.map((item) => (
-              <Tr key={item.id}>
+          <Tbody style={{ color: "midnightblue" }}>
+            {currentData.map((item) => (
+              <Tr key={item._id}>
                 <Td>
-                  <Image src={item.image} className="w-16" />
+                  <Image
+                    src={`http://localhost:8000/images/${item.images[0]}`}
+                    className="h-24 w-24 object-contain"
+                    alt={item.name}
+                  />
                 </Td>
-                <Td>{item.name}</Td>
-                <Td>{item.category}</Td>
+                <Td>{truncateText(item.name, 70)}</Td>
+                <Td style={{ textAlign: "center" }}>{item.subcategory}</Td>
                 <Td>
-                  <div className="flex items-center gap-x-4">
-                    <button>
-                      <FcEditImage className="text-3xl" />
-                    </button>
-                    <button>
-                      <FcEmptyTrash className="text-3xl" />
-                    </button>
-                  </div>
+                  <HStack spacing={2}>
+                    <FcEditImage size={36} />
+                    <FcEmptyTrash size={36} />
+                  </HStack>
                 </Td>
               </Tr>
             ))}
           </Tbody>
         </Table>
-        <div className="mt-4 flex justify-center">
-          {[...Array(totalPages)].map((_, i) => (
-            <button
-              key={i}
-              className={`mx-2 ${
-                i === currentPage ? "bg-blue-500 text-white" : "bg-gray-200"
-              } rounded-md px-3 py-1`}
-              onClick={() => handlePageChange(i)}
-            >
-              {i + 1}
-            </button>
-          ))}
+        <div className="flex justify-center pt-4">
+          <button
+            disabled={currentPage === 1}
+            onClick={handlePrevPage}
+            className="mx-2 rounded bg-blue-300 p-2 font-bold text-white"
+          >
+            صفحه قبلی
+          </button>
+          {
+            <Text className="flex items-center text-2xl text-blue-400">
+              {currentPage}
+            </Text>
+          }
+          <button
+            disabled={currentPage === maxPages}
+            onClick={handleNextPage}
+            className="mx-2 rounded bg-blue-300 p-2 font-bold text-white"
+          >
+            صفحه بعدی
+          </button>
         </div>
       </TableContainer>
     </>
