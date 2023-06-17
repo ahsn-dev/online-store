5b406e2b6a52a9d9d27f2375dec2e3c162a01790
ahsn.dev <ahsn.dev@gmail.com>
2023-06-17 09:35:01 +0330

    refactor with useMutation

--- src/components/DeleteProductModal.tsx
+++ src/components/DeleteProductModal.tsx
@@ -8,20 +8,79 @@ import {
   ModalFooter,
   Text,
 } from "@chakra-ui/react";
-import { QueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { useState } from "react";
 import { FcEmptyTrash } from "react-icons/fc";
+import {
+  QueryClient,
+  QueryObserverResult,
+  RefetchOptions,
+  RefetchQueryFilters,
+  useMutation,
+} from "@tanstack/react-query";
+import { Product } from "../entities/ProductsPanel";
 
 interface Props {
   itemId: string;
   queryKey: string; // the query key used to fetch the product list
   queryClient: QueryClient; // the query client instance
+  currentPage: number;
+  checkProductTotalPage: boolean;
+  refetch: <TPageData>(
+    options?: (RefetchOptions & RefetchQueryFilters<TPageData>) | undefined
+  ) => Promise<
+    QueryObserverResult<
+      {
+        products: Product[];
+        totalProducts: number;
+        totalPage: number;
+      },
+      unknown
+    >
+  >;
+  setCurrentPage: (page: number) => void;
+  productsLength: number;
 }
 
-const DeleteProductModal = ({ itemId, queryKey, queryClient }: Props) => {
+const DeleteProductModal = ({
+  itemId,
+  queryKey,
+  queryClient,
+  currentPage,
+  setCurrentPage,
+  checkProductTotalPage,
+  refetch,
+  productsLength,
+}: Props) => {
   const [isDeleteModalOpen, setDeleteModalOpen] = useState(false);
   const [deleteProductId, setDeleteProductId] = useState("");
+
+  const mutation = useMutation(
+    async (productId: string) => {
+      await axios.delete(`http://localhost:8000/api/products/${productId}`, {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NzZmZGE0ODA3MjkyNTdiOTExOTBhNCIsImlhdCI6MTY4NjQ5Nzc1NywiZXhwIjoxNjg5MDg5NzU3fQ.EBYv0Kj6kD1oFbhmsULf4Q6B-eAX0dU-A5bAbj0VBT4",
+        },
+      });
+    },
+    {
+      onSuccess: async () => {
+        await queryClient.invalidateQueries([queryKey]);
+
+        if (checkProductTotalPage) {
+          refetch();
+        } else {
+          if (productsLength < 1 && currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+          }
+        }
+      },
+      onError: (error: any) => {
+        console.error("Error deleting product:", error);
+      },
+    }
+  );
 
   const handleDeleteProduct = async (productId: string) => {
     setDeleteModalOpen(true);
@@ -30,17 +89,7 @@ const DeleteProductModal = ({ itemId, queryKey, queryClient }: Props) => {
 
   const confirmDeleteProduct = async () => {
     try {
-      await axios.delete(
-        `http://localhost:8000/api/products/${deleteProductId}`,
-        {
-          headers: {
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NzZmZGE0ODA3MjkyNTdiOTExOTBhNCIsImlhdCI6MTY4NjQ5Nzc1NywiZXhwIjoxNjg5MDg5NzU3fQ.EBYv0Kj6kD1oFbhmsULf4Q6B-eAX0dU-A5bAbj0VBT4",
-          },
-        }
-      );
-      await queryClient.invalidateQueries([queryKey]); // invalidate the query to refetch the data
-      //   refetch();
+      await mutation.mutateAsync(deleteProductId);
       setDeleteModalOpen(false);
     } catch (error) {
       console.error("Error deleting product:", error);
