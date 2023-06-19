8a0d50031dabc33e7650f46958b245c0eb4dfbd9
ahsn.dev <ahsn.dev@gmail.com>
2023-06-19 09:50:42 +0330

    update my removeFromCart functionality

--- src/store.ts
+++ src/store.ts
@@ -36,9 +36,28 @@ const useCartStore = create<CartStore>((set) => ({
     });
   },
   removeFromCart: (itemId) => {
-    set((state) => ({
-      cartItems: state.cartItems.filter((i) => i.id !== itemId),
-    }));
+    set((state) => {
+      const itemIndex = state.cartItems.findIndex((i) => i.id === itemId);
+      if (itemIndex !== -1) {
+        const item = state.cartItems[itemIndex];
+        if (item.quantity === 1) {
+          // Remove the item from the cart if there is only one
+          return {
+            cartItems: state.cartItems.filter(
+              (i, index) => index !== itemIndex
+            ),
+          };
+        } else {
+          // Decrease the quantity by one if there are multiple items
+          return {
+            cartItems: state.cartItems.map((i, index) =>
+              index === itemIndex ? { ...i, quantity: i.quantity - 1 } : i
+            ),
+          };
+        }
+      }
+      return state; // Return the current state if the item was not found
+    });
   },
 }));
 
