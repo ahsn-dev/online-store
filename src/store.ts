bc2a51350bf0ffe5d675bec51bcdb9a5bf214e17
ahsn.dev <ahsn.dev@gmail.com>
2023-06-19 08:47:35 +0330

    add useCartStore for cart functionality

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,45 @@
 import { create } from "zustand";
 
-const useOnlineStore = create((set) => ({}));
+// Define the shape of the cart item
+export interface CartItem {
+  id: string;
+  name: string;
+  image: string;
+  price: number;
+  quantity: number;
+}
 
-export default useOnlineStore;
+// Define the shape of the cart store
+export interface CartStore {
+  cartItems: CartItem[];
+  addToCart: (item: CartItem) => void;
+  removeFromCart: (itemId: string) => void;
+}
+
+// Create the cart store
+const useCartStore = create<CartStore>((set) => ({
+  cartItems: [],
+  addToCart: (item) => {
+    set((state) => {
+      const existingItem = state.cartItems.find((i) => i.id === item.id);
+      if (existingItem) {
+        return {
+          cartItems: state.cartItems.map((i) =>
+            i.id === item.id ? { ...i, quantity: i.quantity + 1 } : i
+          ),
+        };
+      } else {
+        return {
+          cartItems: [...state.cartItems, { ...item, quantity: 1 }],
+        };
+      }
+    });
+  },
+  removeFromCart: (itemId) => {
+    set((state) => ({
+      cartItems: state.cartItems.filter((i) => i.id !== itemId),
+    }));
+  },
+}));
+
+export default useCartStore;
