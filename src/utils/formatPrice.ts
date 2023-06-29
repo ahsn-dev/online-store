9ae26961abf2902a2ded92489f9105c200d7bf40
ahsn.dev <ahsn.dev@gmail.com>
2023-06-29 16:07:32 +0330

    add formatPriceFa

--- src/utils/formatPrice.ts
+++ src/utils/formatPrice.ts
@@ -1,3 +1,8 @@
 export const formatPrice = (price: number): string => {
   return price?.toLocaleString();
 };
+
+export const formatPriceFa = (price: number): string => {
+  const formatter = new Intl.NumberFormat("fa-IR");
+  return formatter.format(price);
+};
