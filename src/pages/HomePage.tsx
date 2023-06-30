33f8e8a7f7312db7994cdc7486491201bac986f6
ahsn.dev <ahsn.dev@gmail.com>
2023-06-30 14:18:52 +0330

    add scrollTo in useEffect

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,8 +2,13 @@ import Slider from "../components/Slider";
 import Info from "../components/Info";
 import ProductsCategory from "../components/ProductsCategory";
 import LatestProducts from "../components/LatestProducts";
+import { useEffect } from "react";
 
 const HomePage = () => {
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  });
+
   return (
     <>
       <Slider />
