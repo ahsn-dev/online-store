192c266b6b5151f54bfa220f5ed7bee44c364071
ahsn.dev <ahsn.dev@gmail.com>
2023-06-30 14:24:30 +0330

    move create data to successful payment

--- src/pages/FinalizePurchase.tsx
+++ src/pages/FinalizePurchase.tsx
@@ -1,5 +1,233 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
+// import { useState } from "react";
+// import { useForm } from "react-hook-form";
+// import { yupResolver } from "@hookform/resolvers/yup";
+// import * as yup from "yup";
+// import {
+//   Box,
+//   Button,
+//   Flex,
+//   FormControl,
+//   FormErrorMessage,
+//   FormLabel,
+//   Input,
+//   Stack,
+// } from "@chakra-ui/react";
+// import { FcViewDetails } from "react-icons/fc";
+// import { OrderFormData } from "../entities/OrderFormData";
+// import useCartStore from "../store";
+// import { v4 as uuidv4 } from "uuid";
+// import { Faker, fa } from "@faker-js/faker";
+// const faker = new Faker({ locale: [fa] });
+
+// const schema = yup.object().shape({
+//   firstname: yup.string().required("نام الزامی است"),
+//   lastname: yup.string().required("نام خانوادگی الزامی است"),
+//   address: yup.string().required("آدرس الزامی است"),
+//   phoneNumber: yup.string().required("شماره تماس الزامی است"),
+//   deliveryDate: yup.string().required("تاریخ تحویل الزامی است"),
+// });
+
+// const FinalizePurchase = () => {
+//   const [isLoading, setIsLoading] = useState(false);
+
+//   const {
+//     handleSubmit,
+//     register,
+//     formState: { errors },
+//   } = useForm<OrderFormData>({
+//     resolver: yupResolver(schema),
+//   });
+
+//   const onSubmit = async (formData: OrderFormData) => {
+//     setIsLoading(true);
+
+//     const { firstname, lastname, address, phoneNumber, deliveryDate } =
+//       formData;
+
+//     const createUser = async () => {
+//       const user = {
+//         firstname: firstname,
+//         lastname: lastname,
+//         username: faker.internet.userName(),
+//         password: uuidv4(),
+//         phoneNumber: phoneNumber,
+//         address: address,
+//         role: "USER",
+//       };
+
+//       try {
+//         const response = await fetch("http://localhost:8000/api/users", {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//             Authorization:
+//               "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NzZmZGE0ODA3MjkyNTdiOTExOTBhNCIsImlhdCI6MTY4NzYyMTA2OCwiZXhwIjoxNjkwMjEzMDY4fQ.4Md-7MchA4UtX1DZ2ecTffeBHWmQ7sfpt5ukc4K_0QM",
+//           },
+//           body: JSON.stringify(user),
+//         });
+
+//         const data = await response.json();
+//         return data.data.user._id;
+//       } catch (error) {
+//         console.error(error, "Failed to create user.");
+//       }
+//     };
+
+//     const createOrder = async (userId: string) => {
+//       const cartItems = useCartStore.getState().cartItems;
+
+//       const products = cartItems.map((item) => ({
+//         product: item.id,
+//         count: item.quantity,
+//       }));
+
+//       const order = {
+//         user: userId,
+//         products: products,
+//         deliveryStatus: false,
+//         deliveryDate: deliveryDate,
+//       };
+
+//       try {
+//         const response = await fetch("http://localhost:8000/api/orders", {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           body: JSON.stringify(order),
+//         });
+
+//         const data = await response.json();
+//         return data.data.order._id; // Return the orderId
+//       } catch (error) {
+//         console.error(error, "error");
+//       }
+//     };
+
+//     const userId = await createUser();
+//     if (userId) {
+//       const orderId = await createOrder(userId);
+//       window.location.href = `http://localhost:3003/payment?orderId=${orderId}`;
+//     }
+//     setIsLoading(false);
+//   };
+
+//   // Get the current date in the format required by the input field
+//   const today = new Date().toISOString().split("T")[0];
+
+//   return (
+//     <Box marginTop={8}>
+//       <Box
+//         as="h1"
+//         fontSize="2xl"
+//         fontWeight="bold"
+//         mb={16}
+//         paddingRight={28}
+//         display={"flex"}
+//         alignItems={"center"}
+//         gap={2}
+//       >
+//         <FcViewDetails className="text-4xl" />
+//         نهایی کردن خرید
+//       </Box>
+
+//       <form onSubmit={handleSubmit(onSubmit)}>
+//         <Stack spacing={6} width="60%" marginX="auto">
+//           <Flex gap={4}>
+//             <Box flex="1" mr={3}>
+//               <FormControl isInvalid={!!errors.firstname}>
+//                 <FormLabel htmlFor="firstname">نام</FormLabel>
+//                 <Input
+//                   type="text"
+//                   id="firstname"
+//                   {...register("firstname")}
+//                   placeholder="نام خود را وارد کنید"
+//                 />
+//                 <FormErrorMessage>{errors.firstname?.message}</FormErrorMessage>
+//               </FormControl>
+//             </Box>
+//             <Box flex="1" ml={3}>
+//               <FormControl isInvalid={!!errors.lastname}>
+//                 <FormLabel htmlFor="lastname">نام خانوادگی</FormLabel>
+//                 <Input
+//                   type="text"
+//                   id="lastname"
+//                   {...register("lastname")}
+//                   placeholder="نام خانوادگی خود را وارد کنید"
+//                 />
+//                 <FormErrorMessage>{errors.lastname?.message}</FormErrorMessage>
+//               </FormControl>
+//             </Box>
+//           </Flex>
+
+//           <Flex gap={4}>
+//             <Box flex="1" mr={3}>
+//               <FormControl isInvalid={!!errors.address}>
+//                 <FormLabel htmlFor="address">آدرس</FormLabel>
+//                 <Input
+//                   type="text"
+//                   id="address"
+//                   {...register("address")}
+//                   placeholder="آدرس خود را وارد کنید"
+//                 />
+//                 <FormErrorMessage>{errors.address?.message}</FormErrorMessage>
+//               </FormControl>
+//             </Box>
+//             <Box flex="1" ml={3}>
+//               <FormControl isInvalid={!!errors.phoneNumber}>
+//                 <FormLabel htmlFor="phoneNumber">شماره تماس</FormLabel>
+//                 <Input
+//                   type="tel"
+//                   id="phoneNumber"
+//                   {...register("phoneNumber")}
+//                   placeholder="شماره تماس خود را وارد کنید"
+//                 />
+//                 <FormErrorMessage>
+//                   {errors.phoneNumber?.message}
+//                 </FormErrorMessage>
+//               </FormControl>
+//             </Box>
+//           </Flex>
+
+//           <Flex>
+//             <Box flex="1" mr={3}>
+//               <FormControl isInvalid={!!errors.deliveryDate}>
+//                 <FormLabel htmlFor="deliveryDate">تاریخ تحویل</FormLabel>
+//                 <Input
+//                   type="date"
+//                   id="deliveryDate"
+//                   {...register("deliveryDate")}
+//                   min={today} // Set the minimum date to today's date
+//                   width="49%"
+//                 />
+//                 <FormErrorMessage>
+//                   {errors.deliveryDate?.message}
+//                 </FormErrorMessage>
+//               </FormControl>
+//             </Box>
+//           </Flex>
+
+//           <Button
+//             type="submit"
+//             isLoading={isLoading}
+//             loadingText="در حال پردازش پرداخت..."
+//             colorScheme="cyan"
+//             width="40%"
+//             marginX="auto"
+//             marginTop="2rem"
+//           >
+//             پرداخت
+//           </Button>
+//         </Stack>
+//       </form>
+//     </Box>
+//   );
+// };
+
+// export default FinalizePurchase;
+
+import { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import {
@@ -17,6 +245,15 @@ import { OrderFormData } from "../entities/OrderFormData";
 import useCartStore from "../store";
 import { v4 as uuidv4 } from "uuid";
 import { Faker, fa } from "@faker-js/faker";
+import persian from "react-date-object/calendars/persian";
+import persian_fa from "react-date-object/locales/persian_fa";
+import weekends from "react-multi-date-picker/plugins/highlight_weekends";
+import DatePicker, { DateObject } from "react-multi-date-picker";
+
+interface MyDateObject extends DateObject {
+  unix: number;
+}
+
 const faker = new Faker({ locale: [fa] });
 
 const schema = yup.object().shape({
@@ -34,8 +271,14 @@ const FinalizePurchase = () => {
     handleSubmit,
     register,
     formState: { errors },
+    control,
+    setValue,
   } = useForm<OrderFormData>({
     resolver: yupResolver(schema),
+  });
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
   });
 
   const onSubmit = async (formData: OrderFormData) => {
@@ -85,35 +328,37 @@ const FinalizePurchase = () => {
         user: userId,
         products: products,
         deliveryStatus: false,
-        deliveryDate: deliveryDate,
+        deliveryDate: +deliveryDate * 1000,
       };
 
-      try {
-        const response = await fetch("http://localhost:8000/api/orders", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(order),
-        });
+      localStorage.setItem("order", JSON.stringify(order));
 
-        const data = await response.json();
-        return data.data.order._id; // Return the orderId
-      } catch (error) {
-        console.error(error, "error");
-      }
+      // try {
+      //   const response = await fetch("http://localhost:8000/api/orders", {
+      //     method: "POST",
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //     },
+      //     body: JSON.stringify(order),
+      //   });
+
+      //   const data = await response.json();
+      //   return data.data.order._id; // Return the orderId
+      // } catch (error) {
+      //   console.error(error, "error");
+      // }
     };
 
     const userId = await createUser();
     if (userId) {
-      const orderId = await createOrder(userId);
-      window.location.href = `http://localhost:3003/payment?orderId=${orderId}`;
+      await createOrder(userId);
+      window.location.href = `http://localhost:3003/payment`;
     }
     setIsLoading(false);
   };
 
   // Get the current date in the format required by the input field
-  const today = new Date().toISOString().split("T")[0];
+  // const today = new Date().toISOString().split("T")[0];
 
   return (
     <Box marginTop={8}>
@@ -191,7 +436,7 @@ const FinalizePurchase = () => {
 
           <Flex>
             <Box flex="1" mr={3}>
-              <FormControl isInvalid={!!errors.deliveryDate}>
+              {/* <FormControl isInvalid={!!errors.deliveryDate}>
                 <FormLabel htmlFor="deliveryDate">تاریخ تحویل</FormLabel>
                 <Input
                   type="date"
@@ -203,6 +448,37 @@ const FinalizePurchase = () => {
                 <FormErrorMessage>
                   {errors.deliveryDate?.message}
                 </FormErrorMessage>
+              </FormControl> */}
+              <FormControl isInvalid={!!errors.deliveryDate}>
+                <FormLabel htmlFor="deliveryDate">تاریخ تحویل</FormLabel>
+                <Controller
+                  control={control}
+                  name="deliveryDate"
+                  rules={{ required: true }} //optional
+                  render={({ field: { name } }) => (
+                    <div className="w-full">
+                      <DatePicker
+                        name={name}
+                        className="w-full "
+                        calendar={persian}
+                        locale={persian_fa}
+                        minDate={new Date().setDate(31)}
+                        plugins={[weekends()]}
+                        inputClass="custom-input"
+                        placeholder="تاریخ تحویل را انتخاب کنید"
+                        onChange={(date) =>
+                          setValue(
+                            "deliveryDate",
+                            (date as MyDateObject)?.unix?.toString()
+                          )
+                        }
+                      />
+                      <FormErrorMessage>
+                        {errors.deliveryDate?.message}
+                      </FormErrorMessage>
+                    </div>
+                  )}
+                />
               </FormControl>
             </Box>
           </Flex>
